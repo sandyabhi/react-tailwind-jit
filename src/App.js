a801d4843800e048c75a2de91d33f0adf279ee5b
Sandyabhi <abhisandy47@gmail.com>
2021-08-27 15:20:59 +0530

    s

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-green-400 h-screen grid">
+      <div className="w-3/4 my-auto ml-20">
+        <h1 className="text-5xl font-bold mb-10 text-white">JIT mode</h1>
+        <p className="text-white">
+    if the background in green its working
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
